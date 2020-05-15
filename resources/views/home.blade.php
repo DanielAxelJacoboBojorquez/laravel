25b8d3b3b8b4cda8c6a6b83665e85348b45ebb1a
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-05-15 04:48:29 -0700

    test 14

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,7 @@
                 </button>
             </div>
         <div class="modal-body">
-        <form action="{{route('home.store')}}" method="POST">
+        <form action="{{route('home')}}" method="POST">
         {{ csrf_field()}}
             <div class="form-group">
                 <label for="title" class="col-form-label">Title:</label>
