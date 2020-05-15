4915803e39e4a8796de5d3044c46ec7ab9117a76
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-05-15 03:11:28 -0700

    test 8

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,32 @@
                         </div>
                     @endif
 
-                    
+                    <div class="card">
+                        <div class="card-header border-transparent">
+                        <h3 class="card-title">List of Programming Languages</h3>
+
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table m-0">
+                            <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Image</th>
+                                <th>Description</th> 
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                    <tbody>
                 </div>
             </div>
         </div>
