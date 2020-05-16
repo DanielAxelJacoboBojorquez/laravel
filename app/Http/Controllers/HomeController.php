56008d09375b8532aefa90b1a7f5ca1923ed77ef
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-05-15 22:02:06 -0700

    vf3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,6 +28,12 @@ class HomeController extends Controller
         return view('home', ['languages' => $languages]);
     }
 
+    public function edit($languageId)
+    {
+        $language = Language::find($languageId);
+        return view('home.edit', ['language' => $language]);
+    }
+
     public function store(Request $request)
     {
         $new_language = new Language();
@@ -45,5 +51,39 @@ class HomeController extends Controller
         $new_language->save();
 
         return redirect()->route('home'); 
+    }
+
+    public function update(Request $request, $languageId) {
+        $language = Language::find($languageId);
+    
+        $rules = [
+            'title' => 'required|',
+            'description' => 'required',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        if($request->hasFile('image'))
+        {
+            $file = $request->file('image');
+            $random_name = time();
+            $destinationPath = 'images/';
+            $extension = $file->getClientOriginalExtension();
+            $filename = $random_name.'-'.$file->getClientOriginalName();
+            $uploadSuccess = $request->file('image')->move($destinationPath, $filename);
+            $language->image = $filename;
+        }
+
+        $language->title = $request->title;
+        $language->description = $request->description;
+
+        $language->save();
+
+        return redirect()->route('home');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,9 +17,8 @@
                     <div class="card">
                         <div class="card-header border-transparent">
                         <h2 class="card-title">List of Programming Languages
-                        <button type="button" class="ml-4 btn btn-sm btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Add</button>
+                            <button type="button" class="ml-4 btn btn-sm btn-primary" data-toggle="modal" data-target="#add" data-whatever="@getbootstrap">Add</button>
                         </h2>
-
                         <div class="card-tools">
                             <button type="button" class="btn btn-tool" data-card-widget="collapse">
                                 <i class="fas fa-minus"></i>
@@ -47,7 +46,14 @@
                                         <td>{{ $language->title }}</td>
                                         <td><img src="{{asset('images/'.$language->image)}}" class="img-fluid img-rounded" width="120px" alt="{{$language->title}}"></td>
                                         <td>{{ $language->description }}</td>
-                                        <td>lol</td>
+                                        <td>
+                                            <div class="row">
+                                                <a href="{{route('home.edit')}}" class="btn btn-warning" data-toggle="modal" data-target="#edit" data-whatever="@getbootstrap">Edit</a>
+                                                <form action="">
+                                                    <button class="btn btn-danger">Delete</button>
+                                                </form>
+                                            </div>
+                                        </td>
                                     </tr>
                                 @endforeach
                                 </tbody>
@@ -60,8 +66,8 @@
     </div>
 </div>
 
-<!-- modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- modal Add-->
+<div class="modal fade" id="add" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
         <div class="modal-header">
@@ -83,6 +89,44 @@
                     </div>
                     <div class="form-group">
                         <label for="image" class="col-form-label">Image:</label>
+                        <input type="file" class="form-control" name="image" id="image">
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div>
+        </form>
+        </div>
+    </div>
+</div>
+<!-- /.modal -->
+
+<!-- modal Edit-->
+<div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Edit Programming Language</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <form action="{{route('home.store')}}" method="POST" enctype="multipart/form-data">
+            {{ csrf_field() }}
+            <div class="modal-body">
+                    <div class="form-group">
+                        <label for="title" class="col-form-label">Title:</label>
+                        <input type="text" class="form-control" name="title" id="title" value="{{$language->title}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="description" class="col-form-label">Description:</label>
+                        <textarea class="form-control" name="description" id="description">{{$language->description}}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="image" class="col-form-label">Image:</label>
+                        <br>
+                        <img src="{{asset('images/'.$language->image)}}" class="img-fluid mb-3" width="120px">
                         <input type="file" class="form-control" name="image" id="image">
                     </div>
             </div>
