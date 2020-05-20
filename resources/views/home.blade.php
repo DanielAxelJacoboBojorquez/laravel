5bd3b06b1de67b5f441d429bea40831943a06ba3
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-05-19 18:43:38 -0700

    bug edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,6 +57,43 @@
                                             </div>
                                         </td>
                                     </tr>
+                                    <!-- modal Edit-->
+                                    <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Edit Programming Language</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <form action="{{route('home.update', $language->id)}}" method="POST" enctype="multipart/form-data">
+            {{ csrf_field() }}
+            <div class="modal-body">
+                    <div class="form-group">
+                        <label for="title" class="col-form-label">Title:</label>
+                        <input type="text" class="form-control" name="title" id="title" value="{{$language->title}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="description" class="col-form-label">Description:</label>
+                        <textarea class="form-control" name="description" id="description">{{$language->description}}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="image" class="col-form-label">Image:</label>
+                        <br>
+                        <img src="{{asset('images/'.$language->image)}}" class="img-fluid mb-3" width="120px">
+                        <input type="file" class="form-control" name="image" id="image">
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Update</button>
+            </div>
+        </form>
+        </div>
+    </div>
+</div>
+<!-- /.modal -->
                                 @endforeach
                                 </tbody>
                             </table>
@@ -104,41 +141,5 @@
 </div>
 <!-- /.modal -->
 
-<!-- modal Edit-->
-<div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Edit Programming Language</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <form action="{{route('home.update', $language->id)}}" method="POST" enctype="multipart/form-data">
-            {{ csrf_field() }}
-            <div class="modal-body">
-                    <div class="form-group">
-                        <label for="title" class="col-form-label">Title:</label>
-                        <input type="text" class="form-control" name="title" id="title" value="{{$language->title}}">
-                    </div>
-                    <div class="form-group">
-                        <label for="description" class="col-form-label">Description:</label>
-                        <textarea class="form-control" name="description" id="description">{{$language->description}}</textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="image" class="col-form-label">Image:</label>
-                        <br>
-                        <img src="{{asset('images/'.$language->image)}}" class="img-fluid mb-3" width="120px">
-                        <input type="file" class="form-control" name="image" id="image">
-                    </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Update</button>
-            </div>
-        </form>
-        </div>
-    </div>
-</div>
-<!-- /.modal -->
+
 @endsection
